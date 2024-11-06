e4a57928516b2939b62c263e7a44efd540780fa6
Matheus Soares <m.souza19@unifesp.br>
2024-11-06 15:49:11 -0300

    update

--- src/Primeiro_programa_java.java
+++ src/Primeiro_programa_java.java
@@ -1,5 +1,5 @@
 public class Primeiro_programa_java {
     public static void main(String[] args) {
-        System.out.println("Olá,mundo");
+        System.out.println("Olá,mundo, como vai");
     }
 }
